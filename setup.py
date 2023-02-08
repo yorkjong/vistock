8c84e205638f0590942c5025092cc7b70f048eb4
York <YorkJong@users.noreply.github.com>
2023-02-08 12:25:36 +0800

    Update setup.py
    Increased the version number

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='vistock',
-    version='0.1',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'pandas',
         'yfinance',
         'mplfinance',
         'plotly',
+        #'kaleido',  # plotly uses this to save picture
     ],
     author='York Jong',
     author_email='york.jong@gmail.com',
