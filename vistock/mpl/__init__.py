4b6e3f2320197f807905b3d61638b0472a060d81
York <YorkJong@users.noreply.github.com>
2023-02-08 11:12:58 +0800

    Extracted pbv.py from ViStock.ipynb

--- vistock/mpl/__init__.py
+++ vistock/mpl/__init__.py
@@ -1,3 +1,4 @@
 __all__ = [
     'rsi',  # Plot a 3-split (price, volume, RSI) stock chart.
+    'pbv',  # Plot a price-by-volume stock chart.
 ]
---
+++ vistock/mpl/pbv.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Visualize a PBV (means price-by-volume, also called volume profile) for a given
+stock. Here the PBV is overlaid with the price subplot (total 2 subplots).
+"""
+import yfinance as yf
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+
+__software__ = "Volume Profile 2-split with mplfinace"
+__version__ = "1.0"
+__author__ = "York <york.jong@gmail.com>"
+__date__ = "2023/02/02 (initial version) ~ 2023/02/06 (last revision)"
+
+
+def plot(ticker='TSLA', period='12mo', ma_days=(5, 10, 20, 50, 150),
+         vma_days=50, total_bins=42, legend_loc='best'):
+    """Visualize a PBV (means price-by-volume, also called volume profile) for a
+    given stock. Here the PBV overlaied with the price subplot. This figure
+    consists of two subplots: a price subplot and a volume subplot. The former
+    includes candlestick, moving average lines, while the latter includes
+    trading volume bar chart and volume moving average line.
+
+    Parameters
+    ----------
+    ticker
+        the ticker name (default is 'TSLA')
+    period
+        the period (default is '12mo' that means 12 monthes)
+    ma_days
+        a sequence to list days of moving averge lines
+    total_bins
+        the number of bins to calculate comulative volume for bins (default=42)
+    vma_days
+        days of volume moving average lines
+    legend_loc
+        the location of the legend (default is 'best')
+        Valid locations are
+            best
+            upper right
+            upper left
+            lower left
+            lower right
+            right
+            center left
+            center right
+            lower center
+            upper center
+            center
+    """
+    # Download stock data
+    df = yf.Ticker(ticker).history(period=period)
+
+    # Add Volume Moving Average
+    vma = mpf.make_addplot(df['Volume'], mav=vma_days,
+                           type='line', linestyle='', color='purple', panel=1)
+
+    # Make a customized color style
+    mc = mpf.make_marketcolors(base_mpf_style='yahoo')
+    s = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=mc)
+
+    # Plot candlesticks MA, volume, volume MA, RSI
+    fig, axes = mpf.plot(
+        df, type='candle', mav=ma_days,     # candlestick and MA
+        volume=True, addplot=[vma],         # volume, volume MA
+        style=s, figsize=(16, 8),
+        returnfig=True
+    )
+    axes[0].legend([f'MA {d}' for d in ma_days], loc=legend_loc)
+    df.index = df.index.strftime('%Y-%m-%d')
+    fig.suptitle(f"{ticker}: {df.index.values[0]}~{df.index.values[-1]}",
+                 y=0.93)
+
+    # Volume Profile (i.e., Price by Volume)
+
+    bin_size = (max(df['High']) - min(df['Low'])) / total_bins
+    bin_round = lambda x: bin_size * round(x / bin_size)
+    bin = df['Volume'].groupby(df['Close'].apply(lambda x: bin_round(x))).sum()
+
+    ax = fig.add_axes(axes[0].get_position())
+    ax.set_axis_off()
+    ax.set_xlim(right=1.2*max(bin.values))
+    ax.barh(
+        y=bin.keys(),       # price
+        width=bin.values,   # bin comulative volume
+        height=0.75*bin_size,
+        align='center',
+        color='cyan',
+        alpha=0.4
+    )
+
+    # Show
+    mpf.show()  # plt.show()
+
+    filename = f'{ticker}_{df.index.values[-1]}_pbv.png'
+    fig.savefig(filename)
+
+
+if __name__ == '__main__':
+    plot('TSLA')
