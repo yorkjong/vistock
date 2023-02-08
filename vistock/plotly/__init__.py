879b7a4b902afe6a46febc9e4073fc8bc28907a8
York <YorkJong@users.noreply.github.com>
2023-02-08 13:15:22 +0800

    Update __init__.py
    Published 'pbv4s' module from the plotly package.

--- vistock/plotly/__init__.py
+++ vistock/plotly/__init__.py
@@ -5,4 +5,5 @@ from . import *
 __all__ = [
     'pv1s',     # Price and volume overlaid stock chart
     'pv2s',     # Price and volume separated stock chart
+    'pbv4s',    # Volume Profile (price-by-volume) stock chart with 4 subplots
 ]
