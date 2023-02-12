8b11ebebdc5888935c19fc2ed5bfd9f233244c4b
York <YorkJong@users.noreply.github.com>
2023-02-12 23:12:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vistock',
-    version='0.2.0',
-    python_requires='>=3.6',
-    packages=find_packages(),
-    install_requires=[
+    name = 'vistock',
+    version = '0.2.1',
+    license = 'MIT',
+    author = 'York Jong',
+    author_email = 'york.jong@gmail.com',
+    description = 'Visualizing Stocks'
+    long_description = open('README.md').read(),
+    python_requires = '>=3.6',
+    packages = find_packages(),
+    install_requires = [
         'pandas',
         'yfinance',
         'mplfinance',
         'plotly',
         #'kaleido',  # plotly uses this to save picture
     ],
-    author='York Jong',
-    author_email='york.jong@gmail.com',
-    description='Visualizing Stocks'
 )
