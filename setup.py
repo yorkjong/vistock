271d6abee585080812baae7986672bd79cc8b8f1
York Jong <yukuan.jiang@gmail.com>
2023-02-13 03:43:32 +0800

    Added version number to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'vistock',
-    version = '0.2.1',
+    version = '0.2.2',
     license = 'MIT',
     author = 'York Jong',
     author_email = 'york.jong@gmail.com',
