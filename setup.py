330984bc6650d95ec91e117ca138d616c162c718
York Jong <yukuan.jiang@gmail.com>
2023-02-15 20:08:20 +0800

    Removed document generating part.

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, find_packages
 
-# this is only necessary when not using setuptools/distribute
-#from sphinx.setup_command import BuildDoc
-#cmdclass = {'build_sphinx': BuildDoc}
-
-
-name = 'vistock'
-version = '0.2.4'
 
 setup(
-    name = name,
-    version = version,
+    name = vistock,
+    version = 0.2.4,
     license = 'MIT',
     author = 'York Jong',
     author_email = 'york.jong@gmail.com',
@@ -25,14 +18,5 @@ setup(
         'plotly',
         #'kaleido',  # plotly uses this to save picture
     ],
-    #cmdclass=cmdclass,
-    command_options = {
-        'build_sphinx': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'release': ('setup.py', version),
-            'source_dir': ('setup.py', 'docs'),
-            'build_dir': ('setup.py', 'docs/_build'),
-        }
-    },
 )
+
