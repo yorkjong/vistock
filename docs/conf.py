10021fa256d1171c9d78f4010fecbb50bc9b0b1d
York Jong <yukuan.jiang@gmail.com>
2023-02-20 18:46:30 +0800

    refactor: refined get_version()

--- docs/conf.py
+++ docs/conf.py
@@ -9,9 +9,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import sys
-import codecs
 import os.path
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -19,17 +17,13 @@ sys.path.insert(0, os.path.abspath('..'))
 def get_version(rel_path):
     """Get version from a file with __version__ assignment statement.
     """
-    def read(rel_path):
-        here = os.path.abspath(os.path.dirname(__file__))
-        with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-            return fp.read()
-
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_path, rel_path), 'r') as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
 
 
 # -- Project information -----------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 from setuptools import setup, find_packages
-import codecs
 import os.path
 
 
 def get_version(rel_path):
     """Get version from a file with __version__ assignment statement.
     """
-    def read(rel_path):
-        here = os.path.abspath(os.path.dirname(__file__))
-        with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-            return fp.read()
-
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_path, rel_path), 'r') as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
