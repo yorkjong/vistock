2eb66eb6349e179237c05cfd4cdade45b00a063c
York Jong <yukuan.jiang@gmail.com>
2024-07-19 10:48:53 +0800

    Added version number to 0.2.6

--- vistock/__init__.py
+++ vistock/__init__.py
@@ -2,9 +2,9 @@
 Initialize vistock package.
 """
 __software__ = "Visualizing Stocks"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "York <york.jong@gmail.com>"
-__date__ = "2023/02/02 (initial version) ~ 2023/02/20 (last revision)"
+__date__ = "2023/02/02 (initial version) ~ 2024/07/19 (last revision)"
 
 __all__ = [
     'mpl',      # plot with mplfinance (using matplotlib internal)
