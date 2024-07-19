4293c7d2123890bc80fcdbbc33a7d114925d1621
York Jong <yukuan.jiang@gmail.com>
2024-07-19 10:43:13 +0800

    Rename pbv2s to prf2s

--- tests/test_plotly.py
+++ tests/test_plotly.py
@@ -4,10 +4,10 @@ Test modules in vistock.plotly package.
 import vistock.plotly as vsp
 
 modules = (
-    vsp.pv1s,     # Price and volume overlaid stock chart
-    vsp.pv2s,     # Price and volume separated stock chart
+    #vsp.pv1s,     # Price and volume overlaid stock chart
+    #vsp.pv2s,     # Price and volume separated stock chart
+    vsp.prf2s,    # Volume Profile (price-by-volume) stock chart with 2 subplots
     vsp.pbv4s,    # Volume Profile (price-by-volume) stock chart with 4 subplots
-    vsp.pbv2s,    # Volume Profile (price-by-volume) stock chart with 2 subplots
 )
 
 for m in modules:
--- vistock/plotly/__init__.py
+++ vistock/plotly/__init__.py
@@ -5,8 +5,8 @@ Initialize vistock.plotly package.
 __all__ = [
     'pv1s',     # Price and volume overlaid stock chart
     'pv2s',     # Price and volume separated stock chart
+    'prf2s',    # Volume Profile (price-by-volume) stock chart with 2 subplots
     'pbv4s',    # Volume Profile (price-by-volume) stock chart with 4 subplots
-    'pbv2s',    # Volume Profile (price-by-volume) stock chart with 2 subplots
     #'fig_util', # Common utility for Plotly figures
 ]
 
