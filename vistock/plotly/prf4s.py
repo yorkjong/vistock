b5d9c4ce9897b9f0a426ed1703504a0a45694572
York Jong <yukuan.jiang@gmail.com>
2024-07-19 12:22:34 +0800

    prf4s: Added the feature of Turnover Profile

--- vistock/plotly/prf4s.py
+++ vistock/plotly/prf4s.py
@@ -1,13 +1,16 @@
 """
-Visualize a PBV (means price-by-volume, also called volume profile) for a given
-stock. Here the PBV occupies a split of a 4-split chart.
+Visualize a profile chart (eigher Volume Profile or Turnover Profile) with
+4-section layout for a given stock.
 """
-__software__ = "Volume Profile 4-split with Plotly"
-__version__ = "1.8"
+__software__ = "Volume Profile with Plotly 2x2 subplots"
+__version__ = "2.0"
 __author__ = "York <york.jong@gmail.com>"
-__date__ = "2023/02/02 (initial version) ~ 2024/07/18 (last revision)"
+__date__ = "2023/02/02 (initial version) ~ 2024/07/19 (last revision)"
 
-__all__ = ['plot']
+__all__ = [
+    'Volume',   # Volume Profile, i.e., PBV (Price-by-Volume) or Volume-by-Price
+    'Turnover', # Turnover Profile
+]
 
 import yfinance as yf
 import pandas as pd
@@ -19,62 +22,9 @@ from .. import file_util
 from . import fig_util as futil
 
 
-def plot(symbol='TSLA', period='12mo', interval='1d',
-         ma_nitems=(5, 10, 20, 50, 150), vma_nitems=50, total_bins=42,
-         hides_nontrading=True, out_dir='out'):
-    """Plot a price-by-volume, PBV (also called volume profile) figure for a
-    given stock. This figure shows the volume distribution across price levels
-    for a stock.
-
-    Here the PBV occupies a split of a 4-split figure. This figure also includes
-    candlesticks, price moving average lines, volume histogram, and a volume
-    moving average line.
-
-    Parameters
-    ----------
-    symbol: str
-        the stock symbol.
-    period: str
-        the period data to download. Valid values are 1d, 5d, 1mo, 3mo, 6mo,
-        1y, 2y, 5y, 10y, ytd, max.
-
-        * d   -- days
-        * mo  -- monthes
-        * y   -- years
-        * ytd -- year to date
-        * max -- all data
-
-    interval: str
-        the interval of an OHLC item. Valid values are 1m, 2m, 5m, 15m, 30m,
-        60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo.
-
-        * m  -- minutes
-        * h  -- hours
-        * wk -- weeks
-        * mo -- monthes
-
-        Intraday data cannot extend last 60 days:
-
-        * 1m - max 7 days within last 30 days
-        * up to 90m - max 60 days
-        * 60m, 1h - max 730 days (yes 1h is technically < 90m but this what
-          Yahoo does)
-
-    ma_nitems: sequence of int
-        a sequence to list the number of data items to calclate moving averges.
-    vma_nitems: int
-        the number of data items to calculate the volume moving average.
-    total_bins: int
-        the number of bins to calculate comulative volume for bins.
-    hides_nontrading: bool
-        decide if hides non-trading time-periods.
-    out_dir: str
-        the output directory for saving figure.
-    """
-    # Download stock data
-    symbol = tw.as_yfinance(symbol)
-    df = yf.Ticker(symbol).history(period=period, interval=interval)
-
+def _plot(df, profile_field='Volume', period='12mo', interval='1d',
+          ma_nitems=(5, 10, 20, 50, 150), vma_nitems=50, total_bins=42,
+          hides_nontrading=True):
     # Initialize empty plot with marginal subplots
     fig = make_subplots(
         rows=2,
@@ -88,7 +38,6 @@ def plot(symbol='TSLA', period='12mo', interval='1d',
         vertical_spacing=0.03,
         figure=go.Figure(layout=go.Layout(height=720))
     )
-    #print(fig)
 
     # Plot the candlestick chart
     candlestick = go.Candlestick(
@@ -123,7 +72,8 @@ def plot(symbol='TSLA', period='12mo', interval='1d',
     # Add Price by Volume (Volume Profile) chart
     bin_size = (max(df['High']) - min(df['Low'])) / total_bins
     bin_round = lambda x: bin_size * round(x / bin_size)
-    bin = df['Volume'].groupby(df['Close'].apply(lambda x: bin_round(x))).sum()
+    bin = df[profile_field].groupby(
+            df['Close'].apply(lambda x: bin_round(x))).sum()
     fig.add_trace(
         go.Bar(
             y=bin.keys(),   # Price
@@ -147,9 +97,6 @@ def plot(symbol='TSLA', period='12mo', interval='1d',
 
     # Update layout
     fig.update_layout(
-        title=f'{symbol} {interval} '
-              f'({df.index.values[0]}~{df.index.values[-1]})',
-        title_x=0.5, title_y=.9,
         legend=dict(yanchor='top', xanchor="left", x=1.069),
 
         xaxis=dict(anchor='free'),
@@ -177,15 +124,172 @@ def plot(symbol='TSLA', period='12mo', interval='1d',
     futil.add_crosshair_cursor(fig)
     futil.add_hovermode_menu(fig)
 
-    # Show the figure
-    fig.show()
+    return fig
 
-    # Write the figure to an HTML file
-    out_dir = file_util.make_dir(out_dir)
-    fn = file_util.gen_fn_info(symbol, interval, df.index.values[-1], __file__)
-    fig.write_html(f'{out_dir}/{fn}.html')
+
+class Volume:
+    """Volume Profile, i.e., PBV (Price-by-Volume) or Volume-by-Price
+    """
+    def plot(symbol='TSLA', period='12mo', interval='1d',
+             ma_nitems=(5, 10, 20, 50, 150), vma_nitems=50, total_bins=42,
+             hides_nontrading=True, out_dir='out'):
+        """Plot a price-by-volume, PBV  (also called volume profile) figure for
+        a given stock. This figure shows the volume distribution across price
+        levels for a stock.
+
+        Here the PBV is overlaied with the price subplot. This figure consists
+        of two subplots: a price subplot and a volume subplot. The former
+        includes candlestick, moving average lines, while the latter includes
+        trading volume bar chart and volume moving average line.
+
+        Parameters
+        ----------
+        symbol: str
+            the stock symbol.
+        period: str
+            the period data to download. Valid values are 1d, 5d, 1mo, 3mo, 6mo,
+            1y, 2y, 5y, 10y, ytd, max.
+
+            * d   -- days
+            * mo  -- monthes
+            * y   -- years
+            * ytd -- year to date
+            * max -- all data
+
+        interval: str
+            the interval of an OHLC item. Valid values are 1m, 2m, 5m, 15m, 30m,
+            60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo.
+
+            * m  -- minutes
+            * h  -- hours
+            * wk -- weeks
+            * mo -- monthes
+
+            Intraday data cannot extend last 60 days:
+
+            * 1m - max 7 days within last 30 days
+            * up to 90m - max 60 days
+            * 60m, 1h - max 730 days (yes 1h is technically < 90m but this what
+              Yahoo does)
+
+        ma_nitems: sequence of int
+            a sequence to list the number of data items to calclate moving
+            averges.
+        vma_nitems: int
+            the number of data items to calculate the volume moving average.
+        total_bins: int
+            the number of bins to calculate comulative volume for bins.
+        hides_nontrading: bool
+            decide if hides non-trading time-periods.
+        out_dir: str
+            the output directory for saving figure.
+        """
+        # Download stock data
+        symbol = tw.as_yfinance(symbol)
+        df = yf.Ticker(symbol).history(period=period, interval=interval)
+
+        # Plot
+        fig = _plot(df, 'Volume', period, interval,
+                    ma_nitems, vma_nitems, total_bins, hides_nontrading)
+        fig.update_layout(
+            title=f'{symbol} {interval} '
+                  f'({df.index.values[0]}~{df.index.values[-1]})',
+            title_x=0.5, title_y=.9,
+        )
+
+        # Show the figure
+        fig.show()
+
+        # Write the figure to an HTML file
+        out_dir = file_util.make_dir(out_dir)
+        fn = file_util.gen_fn_info(symbol, interval, df.index.values[-1],
+                                   'volume_prf')
+        fig.write_html(f'{out_dir}/{fn}.html')
+
+
+class Turnover:
+    """Turnover (price*volume) Profile
+    """
+    def plot(symbol='TSLA', period='12mo', interval='1d',
+             ma_nitems=(5, 10, 20, 50, 150), vma_nitems=50, total_bins=42,
+             hides_nontrading=True, out_dir='out'):
+        """Plot a price-by-volume, PBV  (also called volume profile) figure for
+        a given stock. This figure shows the volume distribution across price
+        levels for a stock.
+
+        Here the PBV is overlaied with the price subplot. This figure consists
+        of two subplots: a price subplot and a volume subplot. The former
+        includes candlestick, moving average lines, while the latter includes
+        trading volume bar chart and volume moving average line.
+
+        Parameters
+        ----------
+        symbol: str
+            the stock symbol.
+        period: str
+            the period data to download. Valid values are 1d, 5d, 1mo, 3mo, 6mo,
+            1y, 2y, 5y, 10y, ytd, max.
+
+            * d   -- days
+            * mo  -- monthes
+            * y   -- years
+            * ytd -- year to date
+            * max -- all data
+
+        interval: str
+            the interval of an OHLC item. Valid values are 1m, 2m, 5m, 15m, 30m,
+            60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo.
+
+            * m  -- minutes
+            * h  -- hours
+            * wk -- weeks
+            * mo -- monthes
+
+            Intraday data cannot extend last 60 days:
+
+            * 1m - max 7 days within last 30 days
+            * up to 90m - max 60 days
+            * 60m, 1h - max 730 days (yes 1h is technically < 90m but this what
+              Yahoo does)
+
+        ma_nitems: sequence of int
+            a sequence to list the number of data items to calclate moving
+            averges.
+        vma_nitems: int
+            the number of data items to calculate the volume moving average.
+        total_bins: int
+            the number of bins to calculate comulative volume for bins.
+        hides_nontrading: bool
+            decide if hides non-trading time-periods.
+        out_dir: str
+            the output directory for saving figure.
+        """
+        # Download stock data
+        symbol = tw.as_yfinance(symbol)
+        df = yf.Ticker(symbol).history(period=period, interval=interval)
+        df['Turnover'] = df['Close'] * df['Volume']
+
+        # Plot
+        fig = _plot(df, 'Turnover', period, interval,
+                    ma_nitems, vma_nitems, total_bins, hides_nontrading)
+        fig.update_layout(
+            title=f'{symbol} {interval} '
+                  f'({df.index.values[0]}~{df.index.values[-1]})',
+            title_x=0.5, title_y=.9,
+            xaxis2=dict(title='Bin Comulative Turnover'),
+        )
+
+        # Show the figure
+        fig.show()
+
+        # Write the figure to an HTML file
+        out_dir = file_util.make_dir(out_dir)
+        fn = file_util.gen_fn_info(symbol, interval, df.index.values[-1],
+                                   'turnover_prf')
+        fig.write_html(f'{out_dir}/{fn}.html')
 
 
 if __name__ == '__main__':
-    plot('TSLA')
+    Volume.plot('TSLA')
+    Turnover.plot('TSLA')
 
