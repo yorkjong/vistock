2fc59f4de8ca3aa2f7177579094c745c8466e1a8
York Jong <yukuan.jiang@gmail.com>
2024-07-19 12:22:41 +0800

    Update __init__.py

--- vistock/plotly/__init__.py
+++ vistock/plotly/__init__.py
@@ -5,8 +5,8 @@ Initialize vistock.plotly package.
 __all__ = [
     'pv1s',     # Price and volume overlaid stock chart
     'pv2s',     # Price and volume separated stock chart
-    'prf2s',    # Volume Profile (price-by-volume) stock chart with 2 subplots
-    'prf4s',    # Volume Profile (price-by-volume) stock chart with 4 subplots
+    'prf2s',    # Profile stock chart with 2 subplots
+    'prf4s',    # Profile stock chart with 4 subplots
     #'fig_util', # Common utility for Plotly figures
 ]
 
