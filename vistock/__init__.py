ac942325c5f0e45e7f43705dd603d254a7d44156
York Jong <yukuan.jiang@gmail.com>
2024-08-07 03:24:12 +0800

    fix: publish ibd, stock_indices

--- vistock/__init__.py
+++ vistock/__init__.py
@@ -2,17 +2,21 @@
 Initialize vistock package.
 """
 __software__ = "Visualizing Stocks"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "York <york.jong@gmail.com>"
-__date__ = "2023/02/02 (initial version) ~ 2024/07/22 (last revision)"
+__date__ = "2023/02/02 (initial version) ~ 2024/08/08 (last revision)"
 
 __all__ = [
-    'mpl',      # plot with mplfinance (using matplotlib internal)
-    'plotly',   # plot with Plotly
-    'tw',       # handle stocks of Taiwan markets
+    'mpl',              # plot with mplfinance (using matplotlib internal)
+    'plotly',           # plot with Plotly
+    'tw',               # handle stocks of Taiwan markets
+    'ibd',              # functions for IBD RS and IBD RS Rating
+    'stock_indices',    # functions for Stock Indices
 ]
 
 from . import mpl
 from . import plotly
 from . import tw
+from . import ibd
+from . import stock_indices
 
