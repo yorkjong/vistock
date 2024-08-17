2564369ec15700cf8da229e3c627f58590dc33ba
York Jong <yukuan.jiang@gmail.com>
2024-08-18 02:30:29 +0800

    fix: Avoid a FutureWarning.
    - FutureWarning: The default fill_method='pad' in Series.pct_change is deprecated and will be removed in a future version. Either fill in any non-leading NA values prior to calling pct_change or specify 'fill_method=None' to not fill NA values.
      ret = closes.pct_change(periods=periods)

--- vistock/ibd.py
+++ vistock/ibd.py
@@ -44,9 +44,9 @@ installed.
 For detailed information on each function, please refer to their individual
 docstrings.
 """
-__version__ = "1.6"
+__version__ = "1.7"
 __author__ = "York <york.jong@gmail.com>"
-__date__ = "2024/08/05 (initial version) ~ 2024/08/16 (last revision)"
+__date__ = "2024/08/05 (initial version) ~ 2024/08/18 (last revision)"
 
 __all__ = [
     'relative_strength',
@@ -188,7 +188,7 @@ def quarters_return(closes, n, interval):
         '1mo': 12//4,   # 12 months in a year
     }[interval]
     periods = min(len(closes) - 1, quarter * n)
-    ret = closes.pct_change(periods=periods)
+    ret = closes.pct_change(periods=periods, fill_method='pad')
 
     return ret.replace([np.inf, -np.inf], np.nan).fillna(0)
 
