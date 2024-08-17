f7a399d00ebcc42bbd6c2dd1fb09fc3e9c7e18ca
York Jong <yukuan.jiang@gmail.com>
2024-08-18 02:20:50 +0800

    feat: publish modules of ibd_rs and ibd_rs_cmp

--- vistock/mpl/__init__.py
+++ vistock/mpl/__init__.py
@@ -3,10 +3,11 @@ Initialize vistock.mpl package.
 """
 
 __all__ = [
-    'rsi',          # A 3-split (price, volume, RSI) stock chart.
-    'profile',      # Price-by-volume stock chart.
-    'bull_draw',    # Bull-run & drawdown stock chart.
-    'ibd_rs',       # IBD RS stock chart.
+    'rsi',          # A 3-split (price, volume, RSI) stock chart
+    'profile',      # Price-by-volume stock chart
+    'bull_draw',    # Bull-run & drawdown stock chart
+    'ibd_rs',       # IBD RS stock chart
+    'ibd_rs_cmp',   # IBD RS Comparison stock chart
 ]
 
 from . import *
--- vistock/plotly/__init__.py
+++ vistock/plotly/__init__.py
@@ -5,11 +5,11 @@ Initialize vistock.plotly package.
 __all__ = [
     'pv1s',         # Price and volume overlaid stock chart
     'pv2s',         # Price and volume separated stock chart
-    'bull_draw',    # Bull-run & drawdown stock chart
-    #'ibd_rs',       # IBD RS stock chart
-    #'ibd_rs_cmp',   # IBD RS Comparison stock chart
     'prf2s',        # Profile stock chart with 2 subplots
     'prf4s',        # Profile stock chart with 4 subplots
+    'bull_draw',    # Bull-run & drawdown stock chart
+    'ibd_rs',       # IBD RS stock chart
+    'ibd_rs_cmp',   # IBD RS Comparison stock chart
 ]
 
 from . import *
