a2d699a8412788fc829c480a0367856d21c9d9f6
York Jong <yukuan.jiang@gmail.com>
2024-08-18 23:17:34 +0800

    fix: use forward fill for pct_change and handle NaNs by filling with 0

--- vistock/ibd.py
+++ vistock/ibd.py
@@ -44,7 +44,7 @@ installed.
 For detailed information on each function, please refer to their individual
 docstrings.
 """
-__version__ = "1.7"
+__version__ = "1.8"
 __author__ = "York <york.jong@gmail.com>"
 __date__ = "2024/08/05 (initial version) ~ 2024/08/18 (last revision)"
 
@@ -188,9 +188,9 @@ def quarters_return(closes, n, interval):
         '1mo': 12//4,   # 12 months in a year
     }[interval]
     periods = min(len(closes) - 1, quarter * n)
-    ret = closes.pct_change(periods=periods, fill_method=None)
-
-    return ret.replace([np.inf, -np.inf], np.nan).fillna(0)
+    ret = closes.pct_change(periods=periods, fill_method='ffill')
+    return ret.fillna(0)
+    #return ret.replace([np.inf, -np.inf], np.nan).fillna(0)
 
 
 #------------------------------------------------------------------------------
@@ -453,7 +453,7 @@ def main(min_percentile=80, out_dir='out'):
         out_dir (str, optional): The output directory to store CSV tables.
             Defaults to 'out'
     '''
-    from stock_indices import get_sox_tickers
+    from .stock_indices import get_sox_tickers
     rank_stock, rank_indust = rankings(get_sox_tickers(), interval='1d')
 
     if rank_stock.empty or rank_indust.empty:
