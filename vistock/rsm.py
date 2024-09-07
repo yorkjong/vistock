fix: Adjusted relative strength calculation to handle division by zero more effectively
Updated the relative strength calculation to use the minimum absolute values between the metric and the moving average for the denominator, ensuring more accurate results and avoiding potential issues with zero or very small values. Added a small constant (+1e-8) to prevent division by zero.

vistock/rsm.py
@@ -41,9 +41,9 @@ See Also:
   how-to-create-the-mansfield-relative-performance-indicator>`_
 
 """
-__version__ = "3.5"
+__version__ = "3.6"
 __author__ = "York <york.jong@gmail.com>"
-__date__ = "2024/08/23 (initial version) ~ 2024/09/07 (last revision)"
+__date__ = "2024/08/23 (initial version) ~ 2024/09/08 (last revision)"
 
 __all__ = [
     'mansfield_relative_strength',
@@ -188,8 +188,10 @@ def relative_strength_vs_benchmark(metric_series, bench_series, window=4):
     avg_bench = bench_series.rolling(window=window, min_periods=1).mean()
 
     # Calculate percentage change relative to the moving average
-    metric_change = (metric_series - avg_metric) / (np.abs(avg_metric) + 1e-8)
-    bench_change = (bench_series - avg_bench) / (np.abs(avg_bench) + 1e-8)
+    metric_change = (metric_series - avg_metric) / (
+            np.minimum(np.abs(avg_metric), np.abs(metric_series)) + 1e-8)
+    bench_change = (bench_series - avg_bench) / (
+            np.minimum(np.abs(avg_bench), np.abs(bench_series)) + 1e-8)
 
     # Calculate Relative Strength and convert to percentage
     rsm_values = (metric_change.values - bench_change.values) * 100